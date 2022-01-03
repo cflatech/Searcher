c69182b43d04ba92427f1c98ff63b951cf7d9bc9
cflat <cflated@gmail.com>
2022-01-03 17:46:51 +0000

    検索apiをsearchに移動

--- server/api/controller.ts
+++ server/api/controller.ts
@@ -1,9 +1,7 @@
 import { defineController } from "./$relay";
-import { search as slackSearch } from "$/repository/sites/slackRepository";
 
 export default defineController(() => ({
-  get: async ({ query }) => {
-    const result = await slackSearch(query.query);
-    return { status: 200, body: JSON.stringify(result) };
+  get: async () => {
+    return { status: 200, body: "ok" };
   },
 }));
--- server/api/search/index.ts
+++ server/api/search/index.ts
@@ -1,5 +1,9 @@
 export type Methods = {
   get: {
+    query: {
+      query: string;
+    };
+
     resBody: string;
   };
 };
