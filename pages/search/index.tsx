db630c8f825f4d07044c5e3fb64908a6f762456b
cflat <cflated@gmail.com>
2022-02-23 15:02:06 +0000

    各パーツの右にmarginを追加

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -10,13 +10,13 @@ const Top = () => {
       </Head>
       <main>
         <header className="flex">
-          <div className="text-3xl font-semibold text-center flex justify-center items-end ">
+          <div className="text-3xl font-semibold text-center flex justify-center items-end mr-2">
             Searcher
           </div>
-          <div className="mt-2">
+          <div className="mt-2 mr-2">
             <SearchText />
           </div>
-          <div className="mt-2">
+          <div className="mt-2 mr-2">
             <Button onClick={() => {}}>検索</Button>
           </div>
         </header>
