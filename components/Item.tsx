a55124eb38c1a1df70fd020f5575a1d98eae6367
cflat <cflated@gmail.com>
2022-02-24 17:22:38 +0000

    Itemの時間表示とリンク追加

--- components/Item.tsx
+++ components/Item.tsx
@@ -8,7 +8,13 @@ type Props = {
 const Item: React.VFC<Props> = (props: Props) => {
   return (
     <>
-      <div className="h-32 border-b-2">{props.searchResult.text}</div>
+      <a href={props.searchResult.link} target="_blank">
+        <div className="h-32 border-b-2">
+          {props.searchResult.text}
+          <br></br>
+          更新：{props.searchResult.timestamp.toFormat("yyyy-MM-dd HH:mm:ss")}
+        </div>
+      </a>
     </>
   );
 };
