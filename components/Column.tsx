601111a591583e149880b12dd6c1c863f9504aca
cflat <cflated@gmail.com>
2022-02-25 22:57:04 +0000

    key追加

--- components/Column.tsx
+++ components/Column.tsx
@@ -12,8 +12,8 @@ const Column: React.VFC<Props> = (props: Props) => {
     <>
       <section className="overflow-y-scroll border-x-2 mt-14 flex-1">
         <div className="border-y-2 h-10 text-2xl">{props.searchTargetName}</div>
-        {props.searchResults.map((searchResult) => {
-          return <Item searchResult={searchResult}></Item>;
+        {props.searchResults.map((searchResult, index) => {
+          return <Item key={index} searchResult={searchResult}></Item>;
         })}
       </section>
     </>
