921a7165ee01a0614a4012bef485b95f98f49838
cflat <cflated@gmail.com>
2022-02-25 23:03:09 +0000

    パディング追加

--- components/Column.tsx
+++ components/Column.tsx
@@ -11,7 +11,9 @@ const Column: React.VFC<Props> = (props: Props) => {
   return (
     <>
       <section className="overflow-y-scroll border-x-2 mt-14 flex-1">
-        <div className="border-y-2 h-10 text-2xl">{props.searchTargetName}</div>
+        <div className="border-y-2 h-10 text-2xl px-2">
+          {props.searchTargetName}
+        </div>
         {props.searchResults.map((searchResult, index) => {
           return <Item key={index} searchResult={searchResult}></Item>;
         })}
--- components/Item.tsx
+++ components/Item.tsx
@@ -9,7 +9,7 @@ const Item: React.VFC<Props> = (props: Props) => {
   return (
     <>
       <a href={props.searchResult.link} target="_blank">
-        <div className="h-32 border-b-2 flex flex-col justify-between">
+        <div className="h-32 border-b-2 flex flex-col justify-between px-2 py-1">
           <div className="line-clamp-3">{props.searchResult.text}</div>
           <div className="align-bottom">
             更新：{props.searchResult.timestamp}
